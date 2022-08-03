b8fc042b382d48ee0e4c47cf9cb602ff369dc03a
Rani-rathod <94355220+Rani-rathod@users.noreply.github.com>
2022-08-03 14:59:44 +0530

    Add files via upload

---
+++ rest.js
@@ -0,0 +1,27 @@
+function sum(a,b,c){
+    console.log(a+b+c);
+}
+sum(1,2,3)
+
+
+
+// function sum (...inputs){
+//     console.log(inputs);
+//     let total = 0;
+//     for (let i of inputs){
+//         total+=i
+//     }
+//     console.log(total)
+// }
+// sum(1,2,3,4,5,6,7,8,9)
+
+
+
+// function fun(a,b,...c){
+//     console.log(`${a} ${b}`);
+//     console.log(c);
+//     console.log(c[0]);
+//     console.log(c.length);
+//     console.log(c.indexOf('Edan'));
+// }
+// fun('Rani','Alima','Parveen','Shivani','Divya');
---
+++ spread.js
@@ -0,0 +1,57 @@
+
+
+// function sum(a,b,c){
+//     console.log(a+b+c)
+// }
+
+// // sum(1,2,3)
+
+// var arrVa1 = [1,2,3]
+// sum.apply(null,arrVa1)
+
+
+
+
+// function sum(a,b,c){
+//     console.log(a+b+c)
+// }
+// var arrVa1 = [1,2,3]
+// // console.log(...arrVa1)
+// sum(...arrVa1)
+
+
+
+
+
+// replace concat()
+
+// let arr1 = [1,2,3];
+// let arr2 = [4,5,6];
+// let arr3 = [7,8,9];
+
+// // arr1 = arr1.concat(arr2);
+// arr1 = [...arr1,...arr2,...arr3]
+// console.log(arr1)
+
+
+
+
+
+
+// // replace copy
+
+// let arrc1 = [1,2,4];
+// // let arrc2 = arrc1;
+// // console.log(arrc2);
+// // arrc2.push(6,8)
+
+// let arrc2 = [...arrc1,6,9];
+// console.log(arrc2)
+// // console.log(arrc1) 
+
+
+
+
+let arr = [1,2,3,4];
+let arr1 = [...arr,5,6];
+console.log(arr1)
